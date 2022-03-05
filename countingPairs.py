16b22ad3a9e374c2dc796af1d8a252783b61b39e
sai-blocktrace <75606167+sai-k02@users.noreply.github.com>
2022-03-05 16:11:16 -0600

    Sat Mar 5

---
+++ countingPairs.py
@@ -0,0 +1,7 @@
+"""
+Counting Pairs
+Given an integer k and a list of integers, coutn the number of distinct valid pairs
+of integers (a, b) in the list for which a + k = b. Two pairs of integers(a, b) and (c, d) are considered distinct
+if at least one element of (a, b) does not also belong to (c, d). Note that the elements in a pair might be the same element in the array. An instance of this is below where k=0;
+https://www.geeksforgeeks.org/count-pairs-difference-equal-k/
+"""
---
+++ growth2Dimensions.py
@@ -0,0 +1,6 @@
+"""
+Growth in 2 Dimensions
+
+finding the maximal value in a grid where you traverse it and add 1 to the elements based on the coordinates given
+https://stackoverflow.com/questions/62253968/how-to-solve-two-dimensional-growth-grid-problem
+"""
